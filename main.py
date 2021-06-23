57cd4a4040b5641667fd7019c4f6f428df1dc6e7
dario <dario3desing@gmail.com>
2021-06-23 23:17:49 +0200

    Step 3
    New field added 'date'.
    New functionallity added 'edit task'.
    UI improved

--- main.py
+++ main.py
@@ -10,12 +10,18 @@ def home():
     todas_las_tareas = db.session.query(Tarea).all()
     return render_template("index.html", lista_de_tareas=todas_las_tareas)
 
+
 @app.route('/crear-tarea', methods=['POST'])
 def crear():
-    tarea = Tarea(contenido=request.form['contenido_tarea'], categoria=request.form['categoria_tarea'], hecha=False) # id no es necesario asignarlo (primary key)
+    tarea = Tarea(contenido=request.form['contenido_tarea'], 
+                categoria=request.form['categoria_tarea'],
+                fecha_limite=request.form['fecha_limite_tarea'], # Parseado a Datetime mas adelante
+                hecha=False,
+                editable=False) # id no es necesario asignarlo (primary key)
     db.session.add(tarea) # Añadir el objeto de Tarea a la base de datos
     db.session.commit() # Ejecutar la operación pendiente de la base de datos
     return  redirect(url_for('home'))
+
 
 @app.route('/eliminar-tarea/<id>')
 def eliminar(id):
@@ -23,12 +29,34 @@ def eliminar(id):
     db.session.commit() # Ejecutar la operación pendiente de la base de datos
     return redirect(url_for('home')) # Esto nos redirecciona a la función home() y si todo ha ido bien, al refrescar, la tarea eliminada ya no aparecera en el listado
 
+
+@app.route('/editar-tarea/<id>')
+def editar(id):
+    tarea = db.session.query(Tarea).filter_by(id=int(id)).first()
+    tarea.editable = True
+    db.session.commit()
+    return redirect(url_for('home'))
+
+
+@app.route('/commit-tarea/<id>', methods=['POST'])
+def commit(id):
+    tarea = db.session.query(Tarea).filter_by(id=int(id)).first()
+    # tarea.contenido = request.form['contenido_tarea']
+    # tarea.categoria = request.form['categoria_tarea']
+    # tarea.fecha_limite = request.form['fecha_limite_tarea']
+    # tarea.editable=False
+    tarea.update(request.form['contenido_tarea'], request.form['categoria_tarea'], request.form['fecha_limite_tarea'])
+    db.session.commit()
+    return  redirect(url_for('home'))
+
+
 @app.route('/tarea-hecha/<id>')
 def hecha(id):
     tarea = db.session.query(Tarea).filter_by(id=int(id)).first()
     tarea.hecha = not(tarea.hecha)
     db.session.commit()
     return redirect(url_for('home'))
+
 
 
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -1,5 +1,7 @@
+from sqlalchemy.sql.sqltypes import DateTime
 import db
 from sqlalchemy import Column, Integer, String, Boolean
+from datetime import datetime
 
 '''
 Creamos una clase llamada Tarea
@@ -11,14 +13,25 @@ class Tarea(db.Base):
     __tablename__ = "tarea"
     id = Column(Integer, primary_key=True) # Identificador único de cada tarea (no puede haber dos tareas con el mismo id, por eso es primary key)
     contenido = Column(String(200), nullable=False) # Contenido de la tarea, un texto de máximo 200 caracteres
-    categoria = Column(String(200), nullable=False) 
+    categoria = Column(String(200), nullable=False)
+    fecha_limite = Column(DateTime, nullable=True)
     hecha = Column(Boolean) # Booleano que indica si una tarea ha sido hecha o no
-    def __init__(self, contenido, categoria, hecha):
+    editable = Column(Boolean)
+    def __init__(self, contenido, categoria, fecha_limite, hecha, editable):
         # Recordemos que el id no es necesario crearlo manualmente, lo añade la base de datos automaticamente
         self.contenido = contenido
         self.categoria = categoria
+        self.fecha_limite = datetime.strptime(fecha_limite, r'%d/%m/%y')
         self.hecha = hecha
+        self.editable = editable
+    def update(self, contenido, categoria, fecha_limite):
+        self.contenido = contenido
+        self.categoria = categoria
+        self.fecha_limite = datetime.strptime(fecha_limite, r'%d/%m/%y')
+        self.editable = False
+    def parsed_datetime(self):
+        return self.fecha_limite.strftime(r'%d/%m/%y')
     def __repr__(self):
-        return "Tarea {}: {} {}({})".format(self.id, self.contenido, self.categoria, self.hecha)
+        return "Tarea {}: {} {} {}({} {})".format(self.id, self.contenido, self.categoria, self.fecha_limite , self.hecha, self.editable)
     def __str__(self):
-        return "Tarea {}: {} {} ({})".format(self.id, self.contenido, self.categoria, self.hecha)
+        return "Tarea {}: {} {} {}({} {})".format(self.id, self.contenido, self.categoria, self.fecha_limite, self.hecha, self.editable)
