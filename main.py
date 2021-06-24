8b8f76b19e8abb64d35b91326b5374249b07bce2
dario <dario3desing@gmail.com>
2021-06-24 22:05:38 +0200

    Improvements
    Exception handling
    Slight UI modifications
    Code cleaned

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def home():
 def crear():
     tarea = Tarea(contenido=request.form['contenido_tarea'], 
                 categoria=request.form['categoria_tarea'],
-                fecha_limite=request.form['fecha_limite_tarea'], # Parseado a Datetime mas adelante
+                fecha_limite=request.form['fecha_limite_tarea'], # Parseado a Datetime en el constructor
                 hecha=False,
                 editable=False) # id no es necesario asignarlo (primary key)
     db.session.add(tarea) # Añadir el objeto de Tarea a la base de datos
@@ -41,10 +41,6 @@ def editar(id):
 @app.route('/commit-tarea/<id>', methods=['POST'])
 def commit(id):
     tarea = db.session.query(Tarea).filter_by(id=int(id)).first()
-    # tarea.contenido = request.form['contenido_tarea']
-    # tarea.categoria = request.form['categoria_tarea']
-    # tarea.fecha_limite = request.form['fecha_limite_tarea']
-    # tarea.editable=False
     tarea.update(request.form['contenido_tarea'], request.form['categoria_tarea'], request.form['fecha_limite_tarea'])
     db.session.commit()
     return  redirect(url_for('home'))
--- models.py
+++ models.py
@@ -11,27 +11,37 @@ Esta clase va a almacenar toda la información referente a una tarea
 '''
 class Tarea(db.Base):
     __tablename__ = "tarea"
-    id = Column(Integer, primary_key=True) # Identificador único de cada tarea (no puede haber dos tareas con el mismo id, por eso es primary key)
+    id = Column(Integer, primary_key=True) # Identificador único de cada tarea (primary key)
     contenido = Column(String(200), nullable=False) # Contenido de la tarea, un texto de máximo 200 caracteres
-    categoria = Column(String(200), nullable=False)
-    fecha_limite = Column(DateTime, nullable=True)
+    categoria = Column(String(200), nullable=False) # Categboria de la tarea, un texto de máximo 200 caracteres
+    fecha_limite = Column(DateTime, nullable=True)  # Fecha de la tarea, en formato DateTime
     hecha = Column(Boolean) # Booleano que indica si una tarea ha sido hecha o no
-    editable = Column(Boolean)
+    editable = Column(Boolean) # Booleano que indica si una tarea se encuentra en estado de edicion o no
+
     def __init__(self, contenido, categoria, fecha_limite, hecha, editable):
-        # Recordemos que el id no es necesario crearlo manualmente, lo añade la base de datos automaticamente
         self.contenido = contenido
         self.categoria = categoria
-        self.fecha_limite = datetime.strptime(fecha_limite, r'%d/%m/%y')
+        self.set_fecha(fecha_limite)
         self.hecha = hecha
         self.editable = editable
+
     def update(self, contenido, categoria, fecha_limite):
         self.contenido = contenido
         self.categoria = categoria
-        self.fecha_limite = datetime.strptime(fecha_limite, r'%d/%m/%y')
+        self.set_fecha(fecha_limite)
         self.editable = False
+
+    def set_fecha(self, fecha): # Introducir la fecha en la base de datos, es un proceso delicado, ya que hay que convertir un string a objeto datetime.
+        try:                    # Si ocurre algun error, introducimos la fecha de hoy
+            self.fecha_limite = datetime.strptime(fecha, r'%d/%m/%Y')
+        except Exception as e:
+            self.fecha_limite = datetime(1,1,1,0,0)
+
     def parsed_datetime(self):
-        return self.fecha_limite.strftime(r'%d/%m/%y')
+        return self.fecha_limite.strftime(r'%d/%m/%Y')
+
     def __repr__(self):
         return "Tarea {}: {} {} {}({} {})".format(self.id, self.contenido, self.categoria, self.fecha_limite , self.hecha, self.editable)
+
     def __str__(self):
         return "Tarea {}: {} {} {}({} {})".format(self.id, self.contenido, self.categoria, self.fecha_limite, self.hecha, self.editable)
